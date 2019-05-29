4b6819e3fa51edb3656dcd09f278f63ea7f6b4ad
KamiloMendez <51164210+KamiloMendez@users.noreply.github.com>
2019-05-29 08:59:16 -0500

    Add files via upload

---
+++ js/ventana.js
@@ -0,0 +1,12 @@
+function abrir(){
+        document.getElementById("log").style.display="block";
+		document.getElementById("carouselExampleIndicators").style.display="none";
+		document.getElementById("conta").style.display="none";
+
+    }
+    
+    function cerrar(){
+        document.getElementById("log").style.display="none";
+		document.getElementById("carouselExampleIndicators").style.display="block";
+		document.getElementById("conta").style.display="block";
+    }
